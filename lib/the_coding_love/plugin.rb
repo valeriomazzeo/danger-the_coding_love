9a3edd41da894e7ebe9db9040e441ada562ed619
Olli Tapaninen <olli.tapaninen@bitfactor.fi>
2019-01-29 08:48:27 +0200

    Support for gifs in <video> tag

--- lib/the_coding_love/plugin.rb
+++ lib/the_coding_love/plugin.rb
@@ -18,22 +18,37 @@ module Danger
       require 'open-uri'
       require 'nokogiri'
 
-      @main_page_doc = Nokogiri::HTML(open("https://thecodinglove.com"))
-      random_gif_page_url = @main_page_doc.at_xpath("//a[@class='nav-link']/@href").to_s
-      
-      @doc = Nokogiri::HTML(open(random_gif_page_url))
-      @doc = @doc.at_xpath("//div[@class='blog-post']")
+      @main_page_doc = Nokogiri::HTML(open('https://thecodinglove.com'))
+      random_love_page_url = @main_page_doc.at_xpath("//a[@class='nav-link']/@href").to_s
 
-      text = @doc.at_xpath("//h1").text
-      image_url = @doc.at_xpath("//div[@class='blog-post-content']/p/img/@src").to_s
+      text, image_url = atUrl(random_love_page_url)
 
       markdown("------\n#{text}\n--\n![alt text](#{image_url} \"thecodinglove.com\")")
 
-      return text, image_url
+      [text, image_url]
+    end
+
+    # Returns text and url containing given post from thecodinglove.com url
+    #
+    # @return  [text, image_url]
+    #
+    def atUrl(love_page_url)
+      @doc = Nokogiri::HTML(open(love_page_url))
+      @doc = @doc.at_xpath("//div[@class='blog-post']")
+
+      text = @doc.at_xpath('//h1').text
+      image_url = @doc.at_xpath("//div[@class='blog-post-content']/p/img/@src").to_s
+      video_gif_url = @doc.at_xpath("//div[@class='blog-post-content']/p/video//object[@type='image/gif']/@data").to_s
+
+      if image_url.empty?
+        return [text, video_gif_url]
+      else
+        return [text, image_url]
+      end
     end
 
     def self.instance_name
-      to_s.gsub("Danger", "").danger_underscore.split("/").last
+      to_s.gsub('Danger', '').danger_underscore.split('/').last
     end
   end
 end

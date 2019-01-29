ee1d173eeb900d1da78237c1abcf96563d6cf7da
Olli Tapaninen <ollitapa@gmail.com>
2019-01-29 11:55:06 +0200

    Add support for parsing gifs in <video> tag (#6)

--- spec/the_coding_love_spec.rb
+++ spec/the_coding_love_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../spec_helper', __FILE__)
+require File.expand_path('spec_helper', __dir__)
 
 module Danger
   describe Danger::DangerTheCodingLove do
@@ -12,11 +12,29 @@ module Danger
         @the_coding_love = @dangerfile.the_coding_love
       end
 
-      it "prints a random post" do
+      it 'prints a random post' do
         post = @the_coding_love.random
 
-        expect(post[0]).not_to be_nil
-        expect(post[1]).not_to be_nil
+        expect(post[0]).not_to be_empty
+        expect(post[1]).not_to be_empty
+        expect(@dangerfile.status_report[:markdowns]).not_to be_nil
+      end
+
+      it 'prints an image post' do
+        post = @the_coding_love.atUrl('https://thecodinglove.com/ai-in-cars-is-getting-better')
+
+        expect(post[0]).not_to be_empty
+        expect(post[1]).not_to be_empty
+        expect(File.extname(post[1]).strip.downcase).to eq '.jpg'
+        expect(@dangerfile.status_report[:markdowns]).not_to be_nil
+      end
+
+      it 'prints a gif post' do
+        post = @the_coding_love.atUrl('https://thecodinglove.com/when-i-commit-push-right-after-someone-has')
+
+        expect(post[0]).not_to be_empty
+        expect(post[1]).not_to be_empty
+        expect(File.extname(post[1]).strip.downcase).to eq '.gif'
         expect(@dangerfile.status_report[:markdowns]).not_to be_nil
       end
     end
